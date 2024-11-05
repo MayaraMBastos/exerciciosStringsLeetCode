b7028a84def511a85d7f589f748509b8e114f1b9
Mayara Martinello Bastos <maybastos2021@gmail.com>
2024-11-05 19:30:51 -0300

    ache a diferenca entre strings

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -10,11 +10,12 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         String s = sc.next();
         String t = sc.next();
-        anagrama(s,t);
-
+       // anagrama(s, t);
+findTheDifference(s,t);
     }
 
-    public static boolean anagrama(String s , String t) {
+    //metodo para descobrir se uma string é um anagrama de outra
+    public static boolean anagrama(String s, String t) {
         // Convertendo as Strings em listas ordenadas
         List<Character> sortedListS = s.chars()
                 .mapToObj(c -> (char) c)
@@ -27,9 +28,32 @@ public class Main {
                 .collect(Collectors.toList());
         // Se a ListS for igual a ListT então elas contem os mesmos caracteres e sao um anagrama
         if (sortedListS.equals(sortedListT)) {
+            System.out.println("true");
             return true;
         }
         return false;
     }
 
+    //metodo para achar qual caractere foi adicionado a uma string
+    public static char findTheDifference(String s, String t) {
+        List<Character> listS = s.chars()
+                .mapToObj(c -> (char) c)
+                .collect(Collectors.toList());
+
+        List<Character> listT = t.chars()
+                .mapToObj(c -> (char) c)
+                .collect(Collectors.toList());
+
+        for (Character lt : listT) {
+            if (!listS.contains(lt)) {
+                System.out.println(lt);
+                return lt;
+            } else {
+                // Remove the character from listS to account for duplicate occurrences
+                listS.remove(lt);
+            }
+        }
+
+        return 'A';
+    }
 }
