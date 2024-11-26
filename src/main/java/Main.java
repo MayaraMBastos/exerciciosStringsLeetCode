92f04b56716df9d20cc4623cb5f5c2e4bf582e5f
Mayara Martinello Bastos <maybastos2021@gmail.com>
2024-11-26 16:32:56 -0300

    metodo para procurar sequencia de genes repetidos

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,33 +8,40 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-       // String s = sc.next();
-        String t = sc.next();
+        // String s = sc.next();
+        //String t = sc.next();
         // anagrama(s, t);
         //findTheDifference(s, t);
-        char s[]={'h','e','l','l','o'};
-        reverseString(s);
+        //char s[] = {'h', 'e', 'l', 'l', 'o' };
+        //reverseString(s);
+
+//        String paragraph = "Bob hit a ball, the hit BALL flew far after it was hit.";
+//        String[] banned = {"hit"};
+//        mostCommonWord(paragraph, banned);
+        String s = "AAAAACCCCCAAAAACCCCCCAAAAAGGGTTT";
+        findRepeatedDnaSequences(s);
+
     }
 
     //metodo para descobrir se uma string é um anagrama de outra
-    public static boolean anagrama(String s, String t) {
-        // Convertendo as Strings em listas ordenadas
-        List<Character> sortedListS = s.chars()
-                .mapToObj(c -> (char) c)
-                .sorted()
-                .collect(Collectors.toList());
-
-        List<Character> sortedListT = t.chars()
-                .mapToObj(c -> (char) c)
-                .sorted()
-                .collect(Collectors.toList());
-        // Se a ListS for igual a ListT então elas contem os mesmos caracteres e sao um anagrama
-        if (sortedListS.equals(sortedListT)) {
-            System.out.println("true");
-            return true;
-        }
-        return false;
-    }
+//    public static boolean anagrama(String s, String t) {
+//        // Convertendo as Strings em listas ordenadas
+//        List<Character> sortedListS = s.chars()
+//                .mapToObj(c -> (char) c)
+//                .sorted()
+//                .collect(Collectors.toList());
+//
+//        List<Character> sortedListT = t.chars()
+//                .mapToObj(c -> (char) c)
+//                .sorted()
+//                .collect(Collectors.toList());
+//        // Se a ListS for igual a ListT então elas contem os mesmos caracteres e sao um anagrama
+//        if (sortedListS.equals(sortedListT)) {
+//            System.out.println("true");
+//            return true;
+//        }
+//        return false;
+//    }
 
     //metodo para achar qual caractere foi adicionado a uma string
     public static char findTheDifference(String s, String t) {
@@ -68,5 +75,43 @@ public class Main {
             t[i] = s[(s.length - 1) - i]; // Copia os elementos em ordem inversa
         }
         System.out.println(Arrays.toString(t));
+    }
+
+    //metodo para achar a palavra mais usada, exeto palavras banidas
+    public static String mostCommonWord(String paragraph, String[] banned) {
+        // se contem a palavra banida
+        int contagem = 1;
+
+        //separar a string em arrey com cada palavra
+        String[] paraArray = paragraph.split(" ");
+        String ban = banned[0];
+
+        // Percorrendo o array e contando as ocorrências
+        for (String palavra : paraArray) {
+            if (palavra.equals(ban)) {
+                contagem++;
+                // deletar palavra
+            }
+        }
+        System.out.println(contagem);
+
+
+        return "resultado";
+    }
+
+    //metodo para achar sequencias de DNA repetidas
+    public static List<String> findRepeatedDnaSequences(String s) {
+
+        //sao sequencias de 10 chars ,dividir em strings com ate 10 chars
+        Set dna = new HashSet(), repeatidas = new HashSet();
+
+        for (int i = 0; i + 9 < s.length(); i++) {
+            String dez = s.substring(i, i + 10);
+            if (!dna.add(dez))
+                repeatidas.add(dez);
+        }
+        System.out.println(repeatidas);
+        return new ArrayList(repeatidas);
+
     }
 }
